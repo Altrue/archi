4598ea74c11f61e36875b031497ab6f782bc6bad
Altrue <altrue@hotmail.fr>
2015-03-25 11:48:24 +0100

    on continue les modifs css

---
+++ views/index.php
@@ -0,0 +1,44 @@
+<html>
+	<header>
+		<link rel="stylesheet" type="text/css" href="../css/main.css" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'/>
+	<header>
+	<body>
+		<div id="main">
+			<div class="block-contenu"><h1>Titre</h1>
+			<h2>Sous-titre</h2></div>
+			<div class="block-contenu"><h3>Sous-sous-titre !</h3>
+			Blablabla lots of blabla lorem ipsum sator arepo tenet opera rotas.
+			<form class="centered">
+				<input type="submit" class="button_submit" value="Se connecter"/>
+				<input type="submit" class="button_cancel" value="Se déconnecter"/>
+			</form></div>
+			<div class="block-contenu"><h2>Test grille graphique</h2></div>
+			<div class="block-contenu">
+				<div class="clock-grid red">Test</div>
+				<div class="clock-grid orange">test</div>
+				<div class="clock-grid yellow">Test</div>
+				<div class="clock-grid green">test</div>
+				<div class="clock-grid blue">Test</div>
+				<div class="clock-grid purple">test</div>
+			</div>
+			<div class="block-contenu"><h2>Test liste</h2></div>
+			<div class="block-contenu no-padding">
+				<div class="clock-list-element">Test</div>
+				<hr>
+				<div class="clock-list-element">Test</div>
+				<hr>
+				<div class="clock-list-element">Test</div>
+			</div>
+		</div>
+	</body>
+	<footer>
+		
+	</footer>
+<?php
+
+?>
+
+<?php
+
+?>
