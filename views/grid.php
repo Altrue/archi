4ecb12b7452103a74cd9be3aa4c2dab197ccf98d
Altrue <altrue@hotmail.fr>
2015-04-01 15:35:10 +0200

    des trucs

---
+++ views/grid.php
@@ -0,0 +1,25 @@
+<html>
+	<header>
+		<link rel="stylesheet" type="text/css" href="../css/main.css" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'/>
+	<header>
+	<body>
+		<div id="main">
+			<div class="top-bar">
+				<table class="table-top-bar">
+					<tr><td class="td-refresh"></td><td class="td-logo"></td><td class="td-plus"></td></tr>
+				</table>
+			</div>
+			<div class="clock-grid orange1"><div class='clock-grid-content'>Test</div></div>
+			<div class="buttom-bar"><span>SWITCH TO LIST VIEW</span></div>
+	</body>
+	<footer>
+		
+	</footer>
+<?php
+
+?>
+
+<?php
+
+?>
